80214743c6ac12913f00f2bc2c062ac72fa05aaf
SirLich <lrkoehler77@gmail.com>
2021-06-03 21:31:42 +0200

    Add code editor

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+	<div class="app">
+		<CodeEditor></CodeEditor>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, onMounted } from 'vue'
+import CodeEditor from './components/CodeEditor.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default defineComponent({
+	name: 'App',
+	data() {
+		return {
+			code: 'TEST',
+			messages: 'Messages',
+		}
+	},
+	components: {
+		CodeEditor,
+	},
+})
 </script>
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+.app {
+	margin: 15px;
+}
+</style>
---
+++ src/components/CodeEditor.vue
@@ -0,0 +1,130 @@
+<template>
+	<button
+		@click="format"
+		class="
+			bg-blue-500
+			hover:bg-blue-700
+			text-white
+			font-bold
+			py-2
+			px-4
+			rounded
+		"
+	>
+		Format
+	</button>
+	<div>
+		<prism-editor
+			class="my-editor height-200 mb-4"
+			v-model="editorCode"
+			:highlight="highlighter"
+			line-numbers
+		></prism-editor>
+	</div>
+</template>
+
+<script>
+// import Prism Editor
+import { PrismEditor } from 'vue-prism-editor' //
+import 'vue-prism-editor/dist/prismeditor.min.css' // import the styles somewhere
+
+// import highlighting library (you can use any library you want just return html string)
+import prism from 'prismjs'
+import 'prismjs/themes/prism-tomorrow.css' // import syntax highlighting styles
+import 'prismjs/components/prism-json'
+import Ajv from 'ajv'
+import { defineComponent, onMounted, ref } from 'vue'
+
+// https://raw.githubusercontent.com/Blockception/Minecraft-bedrock-json-schemas/main/skinpacks/skins.json
+function getJson()
+export default defineComponent({
+	components: {
+		PrismEditor,
+	},
+	methods: {
+		format() {
+			try {
+				this.editorCode = JSON.stringify(
+					JSON.parse(this.editorCode),
+					null,
+					3
+				)
+			} catch (err) {
+				console.log(err.message)
+			}
+		},
+	},
+	setup(props, context) {
+		const editorCode = ref('hey')
+		const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+
+		const schema = {
+			$id: 'http://example.com/schemas/schema.json',
+			type: 'object',
+			properties: {
+				foo: { $ref: 'defs.json#/definitions/int' },
+				bar: { $ref: 'defs.json#/definitions/str' },
+			},
+		}
+
+		const defsSchema = {
+			$id: 'http://example.com/schemas/defs.json',
+			definitions: {
+				int: { type: 'integer' },
+				str: { type: 'string' },
+			},
+		}
+
+		// Add definitions
+		ajv.addSchema(defsSchema)
+
+		// Compile
+		const validate = ajv.compile(schema)
+
+		const data = {
+			foo: 1,
+			cap: 1,
+			bar: 1,
+		}
+
+		const valid = validate(data)
+		if (!valid) {
+			console.log(validate.errors)
+		} else {
+			console.log('BOB')
+		}
+
+		const highlighter = (code) => {
+			return prism.highlight(code, prism.languages.json)
+		}
+		return { editorCode, highlighter }
+	},
+})
+</script>
+
+<style>
+.my-editor {
+	/* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
+	background: #2d2d2d;
+	color: #ccc;
+
+	/* you must provide font-family font-size line-height. Example: */
+	font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+	font-size: 14px;
+	line-height: 1.5;
+	padding: 5px;
+}
+
+/* optional class for removing the outline */
+.prism-editor__textarea:focus {
+	outline: none;
+}
+
+.height-200 {
+	height: 800px;
+}
+
+.mb-4 {
+	margin-bottom: 1rem;
+}
+</style>
